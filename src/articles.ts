2128041370f63798289fcf8cd85bdb2f67fe490f
Luciano Antonio Santillan <lucianoasa@ext.inditex.com>
2023-06-07 18:15:28 +0200

    init

---
+++ src/articles.ts
@@ -0,0 +1,13 @@
+interface Article {
+    id: number;
+    title: string;
+    description: string;
+}
+
+const articles: Article[] = [
+    { id: 1, title: 'Article 1', description: 'This is the content of Article 1.' },
+    { id: 2, title: 'Article 2', description: 'This is the content of Article 2.' },
+    { id: 3, title: 'Article 3', description: 'This is the content of Article 3.' }
+];
+
+export default articles;
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import articles from './articles';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/articles', (req, res) => {
+  res.json(articles);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
