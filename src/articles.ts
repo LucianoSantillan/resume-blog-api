7bc641ff9e5f478ac70a13ba3678eec6928d7c59
Luciano Antonio Santillan <lucianoasa@ext.inditex.com>
2023-06-23 14:21:12 +0200

    fix: consuming data from environment variables

--- src/articles.ts
+++ src/articles.ts
@@ -3,11 +3,3 @@ interface Article {
     title: string;
     description: string;
 }
-
-const articles: Article[] = [
-    { id: 1, title: 'Article 1', description: 'This is the content of Article 1.' },
-    { id: 2, title: 'Article 2', description: 'This is the content of Article 2.' },
-    { id: 3, title: 'Article 3', description: 'This is the content of Article 3.' }
-];
-
-export default articles;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,54 @@
 import express from 'express';
-import articles from './articles';
 import cors from 'cors';
+import "reflect-metadata";
+import { DataSource, createConnection } from "typeorm";
+import { Article } from './entity/article';
+import dotenv from 'dotenv';
 
-const app = express();
-app.use(cors());
+dotenv.config();
 
-const port = 3000;
+//TEST ENVIRONMENT VARIABLES BEFORE USING THEM
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const AppDataSource = new DataSource({
+  "type": "mysql",
+  "host": process.env.DB_HOST,
+  "port": parseInt(process.env.DB_PORT || "0"),
+  "username": "root",
+  "password": process.env.DB_PASSWORD,
+  "database": "blog",
+  "synchronize": true,
+  "logging": false,
+  "entities": [
+    "src/entity/**/*.ts"
+  ]
+})
 
-app.get('/articles', (req, res) => {
-  res.json(articles);
-});
+AppDataSource.initialize()
+  .then((dataSource) => {
+    const app = express();
+    app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+    const port = 3001;
+
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
+
+    app.get('/articles/:id', async (req, res) => {
+      const articleRepository = dataSource.getRepository(Article);
+      const article = await articleRepository.findOne({ where: { id: parseInt(req.params.id) } });
+      console.log(article)
+      res.json(article);
+    });
+
+    app.get('/articles', async (req, res) => {
+      const articleRepository = dataSource.getRepository(Article);
+      const articles = await articleRepository.find();
+      res.json(articles);
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.log(error))
